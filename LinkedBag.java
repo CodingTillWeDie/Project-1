297acfa18cb56322fcf25e4da89372a722bab205
hemilbhavsar <98733531+hemilbhavsar@users.noreply.github.com>
2022-02-22 23:35:38 -0800

    Add files via upload

---
+++ LinkedBag.java
@@ -0,0 +1,30 @@
+
+public class LinkedBag < T > implements BagInterface {
+	
+	private Node firstNode;
+	
+	private int numberofEntries;
+	
+	public LinkedBag() {
+		firstNode = null;
+		numberofEntries = 0;
+	}
+	//implementation of methods based on bagInterface>>>
+	
+	private class Node{
+		private T data;
+		private Node next;
+		
+		private Node(T dataPortion) {
+			this(dataPortion, null);
+			
+		}
+		private Node(T dataPortion, Node nextNode) {
+			data = dataPortion;
+			next = nextNode;
+		}
+	}
+	
+	
+
+}
