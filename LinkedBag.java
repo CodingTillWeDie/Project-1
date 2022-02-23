fb7af9c5c3e0d57caae2e6214c720f5283907932
kevink0908 <98494454+kevink0908@users.noreply.github.com>
2022-02-22 23:40:56 -0800

    Update LinkedBag.java

--- LinkedBag.java
+++ LinkedBag.java
@@ -2,24 +2,27 @@
 public class LinkedBag < T > implements BagInterface {
 	
 	private Node firstNode;
+	private int numberOfEntries;
 	
-	private int numberofEntries;
-	
-	public LinkedBag() {
+	public LinkedBag() 
+	{
 		firstNode = null;
 		numberofEntries = 0;
 	}
 	//implementation of methods based on bagInterface>>>
 	
-	private class Node{
+	private class Node
+	{
 		private T data;
 		private Node next;
 		
-		private Node(T dataPortion) {
+		private Node(T dataPortion) 
+		{
 			this(dataPortion, null);
 			
 		}
-		private Node(T dataPortion, Node nextNode) {
+		private Node(T dataPortion, Node nextNode) 
+		{
 			data = dataPortion;
 			next = nextNode;
 		}
