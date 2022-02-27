1568d0e3569e64b560252324d6f8479a588e0a92
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-02-27 14:15:53 -0800

    Update ResizableArrayBagTest.java

--- ResizableArrayBagTest.java
+++ ResizableArrayBagTest.java
@@ -1,25 +1,81 @@
-import java.util.*;
+import org.junit.jupiter.api.Test;
 
-public class ResizableArrayBagTest
-{
-    public static void main(String[] args)
-    {
-        BagInterface <String> bag1 = new ResizableArrayBag <>(); //Create two new bags
-        BagInterface <String> bag2 = new ResizableArrayBag <>();
+import static org.junit.jupiter.api.Assertions.*;
 
-        bag1.add("A");
-        bag1.add("B"); //Add the test cases to bag1
-        bag1.add("C");
-        
-        bag2.add("B");
-        bag2.add("B"); //Add the test cases to bag2
-        bag2.add("D");
-        bag2.add("E");
+class ResizableArrayBagTest {
+    BagInterface <String> bag1 = new ResizableArrayBag <>();
+    @Test
+    void union() {
 
-        System.out.println(Arrays.toString(bag1.union(bag2).toArray())); //Prints the union of the two bags
-        System.out.println(Arrays.toString(bag1.intersection(bag2).toArray())); //Prints the intersection of the two bags
-        System.out.println(Arrays.toString(bag1.difference(bag2).toArray())); //Prints the difference of bag1 and bag2
-        System.out.println(Arrays.toString(bag2.difference(bag1).toArray())); //Prints the difference of bag2 and bag1
+    }
 
+    @Test
+    void intersection() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void getCurrentSize() {
+
+    }
+
+    @Test
+    void isEmpty() {
+        if(bag1.isEmpty()){
+            System.out.println("bag is empty");
+        }
+    }
+
+    @Test
+    void isFull() {
+        int DCap = 25;
+        for(int i = 0; i < DCap; i++){
+            bag1.add("A");
+        }
+        if(bag1.isFull()){
+            System.out.println("bag is full");
+        }
+    }
+
+    @Test
+    void add() {
+        int DCap = 25;
+        int numberOfEntries = 0;
+        for(int i = 0; i < DCap; i++){
+            bag1.add("A");
+            numberOfEntries++;
+        }
+
+        if(bag1.isFull()){
+            System.out.println("Number of Entries: " +  numberOfEntries);
+            System.out.println("adding works");
+        }
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void getFrequencyOf() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void toArray() {
     }
 }
