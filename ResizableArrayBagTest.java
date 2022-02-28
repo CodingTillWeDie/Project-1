38b92d2eca31ea81bc28d92a34a44ba314b3e987
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-02-27 16:31:07 -0800

    Update ResizableArrayBagTest.java

--- ResizableArrayBagTest.java
+++ ResizableArrayBagTest.java
@@ -1,24 +1,58 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResizableArrayBagTest {
     BagInterface <String> bag1 = new ResizableArrayBag <>();
+    BagInterface <String> bag2 = new ResizableArrayBag <>();
     @Test
     void union() {
-
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag2.add("B");
+        bag2.add("B");
+        bag2.add("D");
+        bag2.add("E");
+        System.out.println(Arrays.toString(bag1.union(bag2).toArray())); //Prints the union of the two bags
     }
 
     @Test
     void intersection() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag2.add("B");
+        bag2.add("B");
+        bag2.add("D");
+        bag2.add("E");
+        System.out.println(Arrays.toString(bag1.intersection(bag2).toArray())); //Prints the union of the two bags
     }
 
     @Test
     void difference() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag2.add("B");
+        bag2.add("B");
+        bag2.add("D");
+        bag2.add("E");
+        System.out.println("Difference of bag1 to bag2");
+        System.out.println(Arrays.toString(bag1.difference(bag2).toArray())); //Prints the union of the two bags
+        System.out.println("Difference of bag2 to bag1");
+        System.out.println(Arrays.toString(bag2.difference(bag1).toArray())); //Prints the union of the two bags
     }
 
     @Test
     void getCurrentSize() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag1.add("D");
+        System.out.println(Arrays.toString(new int[]{bag1.getCurrentSize()}));
 
     }
 
@@ -30,52 +64,64 @@ class ResizableArrayBagTest {
     }
 
     @Test
-    void isFull() {
-        int DCap = 25;
-        for(int i = 0; i < DCap; i++){
-            bag1.add("A");
-        }
-        if(bag1.isFull()){
-            System.out.println("bag is full");
-        }
-    }
-
-    @Test
     void add() {
-        int DCap = 25;
-        int numberOfEntries = 0;
-        for(int i = 0; i < DCap; i++){
-            bag1.add("A");
-            numberOfEntries++;
-        }
-
-        if(bag1.isFull()){
-            System.out.println("Number of Entries: " +  numberOfEntries);
-            System.out.println("adding works");
-        }
+        bag1.add("A");
+        System.out.println(Arrays.toString(bag1.toArray()));
     }
 
     @Test
     void remove() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.remove("A");
+        System.out.println(Arrays.toString(bag1.toArray()));
     }
 
     @Test
     void testRemove() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.remove();
+        System.out.println(Arrays.toString(bag1.toArray()));
     }
 
     @Test
     void clear() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag1.add("D");
+        bag1.add("E");
+        bag1.add("F");
+        bag1.clear();
+        System.out.println(Arrays.toString(bag1.toArray()));
     }
 
     @Test
     void getFrequencyOf() {
+        bag1.add("A");
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("B");
+        System.out.println(Arrays.toString(new int[]{bag1.getFrequencyOf("A")}));
     }
 
     @Test
     void contains() {
+        bag1.add("A");
+        if(bag1.contains("A")){
+            System.out.println("Bag1 Contains Entry A");
+        }
     }
 
     @Test
     void toArray() {
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag1.add("D");
+        bag1.add("E");
+        bag1.add("F");
+        System.out.println(Arrays.toString(bag1.toArray()));
     }
 }
