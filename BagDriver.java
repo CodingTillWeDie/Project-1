5a5cf68df5905b49375c345d2ea5f46f3dace0c7
kevink0908 <98494454+kevink0908@users.noreply.github.com>
2022-02-28 14:42:04 -0800

    Update BagDriver.java

--- BagDriver.java
+++ BagDriver.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.util.Scanner;
 
-public class BagDriver {
-    public static void main(String[] args) throws NullPointerException {
-
-        int cap = 25;
+public class BagDriver
+{
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException, NullPointerException
+    {
         int choice1;
         int counter = 10000;
         BagInterface<String> bag1 = new ResizableArrayBag<>();
@@ -12,23 +12,37 @@ public class BagDriver {
         BagInterface<String> bag3 = new LinkedBag<>();
         BagInterface<String> bag4 = new LinkedBag<>();
         Scanner keyboard = new Scanner(System.in);
+
+        // prompt the user to enter his or her choice.
         System.out.println("Enter 1 for ResizableArrayBag or 2 for LinkedBag: ");
         choice1 = keyboard.nextInt();
+        // clear the buffer.
         keyboard.nextLine();
-        for(int i = 0; i < counter; i++) {
-            if (choice1 == 1) {
+
+        for (int i = 0; i < counter; i++)
+        {
+            // allow the user to perform union, intersection, and
+            // difference methods on two ResizableArrayBags.
+            if (choice1 == 1)
+            {
                 int choice2;
-                System.out.println("Please enter the number of the desired test case: ");
-                System.out.println("1: Test Case 1 ");
-                System.out.println("2: Test Case 2 ");
-                System.out.println("3: Test Case 3 ");
-                System.out.println("4: Test Case 4 ");
-                System.out.println("5: Test Case 5 ");
-                System.out.println("6: Exit Program ");
+
+                // prompt the user to choose a test case that demonstrates the
+                // execution of the union, intersection, and difference methods.
+                System.out.println("\nPlease enter a number for your desired test case: ");
+                System.out.println("(1) Test Case #1: Two Different Bags");
+                System.out.println("(2) Test Case #2: Two of the Same Bags #1");
+                System.out.println("(3) Test Case #3: Two of the Same Bags #2");
+                System.out.println("(4) Test Case #4: Two Distinct Bags");
+                System.out.println("(5) Test Case #5: Two Empty Bags");
+                System.out.println("(6) Exit the Program");
                 choice2 = keyboard.nextInt();
+                // clear the buffer.
                 keyboard.nextLine();
 
-                if (choice2 == 1) {
+                // check to see if the user wishes to perform the operations on two different bags.
+                if (choice2 == 1)
+                {
                     bag1.clear();
                     bag2.clear();
                     bag1.add("A");
@@ -39,6 +53,9 @@ public class BagDriver {
                     bag2.add("D");
                     bag2.add("E");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag1.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag2.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -47,8 +64,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
-
-                } else if (choice2 == 2) {
+                }
+                // check to see if the user wishes to perform the operations on two of the same bags.
+                else if (choice2 == 2)
+                {
                     bag1.clear();
                     bag2.clear();
                     bag1.add("A");
@@ -60,6 +79,9 @@ public class BagDriver {
                     bag2.add("A");
                     bag2.add("A");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag1.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag2.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -68,8 +90,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
-
-                } else if (choice2 == 3) {
+                }
+                // check to see if the user wishes to perform more operations on two of the same bags.
+                else if (choice2 == 3)
+                {
                     bag1.clear();
                     bag2.clear();
                     bag1.add("A");
@@ -81,6 +105,9 @@ public class BagDriver {
                     bag2.add("B");
                     bag2.add("B");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag1.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag2.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -89,8 +116,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
-
-                } else if (choice2 == 4) {
+                }
+                // check to see if the user wishes to perform the operations on two distinct bags.
+                else if (choice2 == 4)
+                {
                     bag1.clear();
                     bag2.clear();
                     bag1.add("A");
@@ -102,18 +131,9 @@ public class BagDriver {
                     bag2.add("G");
                     bag2.add("H");
 
-                    System.out.print("Result of Union: ");
-                    System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-                    System.out.print("Result of Intersection: ");
-                    System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
-                    System.out.print("Result of Difference from the first bag to the second: ");
-                    System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
-                    System.out.print("Result of Difference from the second bag to the first: ");
-                    System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
-                } else if (choice2 == 5) {
-                    bag1.clear();
-                    bag2.clear();
-
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag1.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag2.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -123,27 +143,55 @@ public class BagDriver {
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
                 }
-                else if (choice2 == 6) {
+                // check to see if the user wishes to perform the operations on two empty bags.
+                else if (choice2 == 5)
+                {
                     bag1.clear();
                     bag2.clear();
 
-                    System.out.println("GoodBye");
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag1.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag2.toArray()));
+                    System.out.print("Result of Union: ");
+                    System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
+                    System.out.print("Result of Intersection: ");
+                    System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+                    System.out.print("Result of Difference from the first bag to the second: ");
+                    System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
+                    System.out.print("Result of Difference from the second bag to the first: ");
+                    System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
+                }
+                // check to see if the user wishes to terminate the program.
+                else if (choice2 == 6)
+                {
+                    bag1.clear();
+                    bag2.clear();
+                    System.out.println("Terminating the program... Goodbye!");
                     break;
                 }
             }
-            else if (choice1 == 2) {
+            // allow the user to perform union, intersection, and
+            // difference methods on two LinkedBags.
+            else if (choice1 == 2)
+            {
                 int choice2;
-                System.out.println("Please enter the number of the desired test case: ");
-                System.out.println("1: Test Case 1 ");
-                System.out.println("2: Test Case 2 ");
-                System.out.println("3: Test Case 3 ");
-                System.out.println("4: Test Case 4 ");
-                System.out.println("5: Test Case 5 ");
-                System.out.println("6: Exit Program ");
+
+                // prompt the user to choose a test case that demonstrates the
+                // execution of the union, intersection, and difference methods.
+                System.out.println("\nPlease enter a number for your desired test case: ");
+                System.out.println("(1) Test Case #1: Two Different Bags");
+                System.out.println("(2) Test Case #2: Two of the Same Bags #1");
+                System.out.println("(3) Test Case #3: Two of the Same Bags #2");
+                System.out.println("(4) Test Case #4: Two Distinct Bags");
+                System.out.println("(5) Test Case #5: Two Empty Bags");
+                System.out.println("(6) Exit the Program");
                 choice2 = keyboard.nextInt();
+                // clear the buffer.
                 keyboard.nextLine();
 
-                if (choice2 == 1) {
+                // check to see if the user wishes to perform the operations on two different bags.
+                if (choice2 == 1)
+                {
                     bag3.clear();
                     bag4.clear();
                     bag3.add("A");
@@ -154,6 +202,9 @@ public class BagDriver {
                     bag4.add("D");
                     bag4.add("E");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag3.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag4.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -162,8 +213,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
-
-                } else if (choice2 == 2) {
+                }
+                // check to see if the user wishes to perform the operations on two of the same bags.
+                else if (choice2 == 2)
+                {
                     bag3.clear();
                     bag4.clear();
                     bag3.add("A");
@@ -175,6 +228,9 @@ public class BagDriver {
                     bag4.add("A");
                     bag4.add("A");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag3.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag4.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -183,8 +239,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
-
-                } else if (choice2 == 3) {
+                }
+                // check to see if the user wishes to perform more operations on two of the same bags.
+                else if (choice2 == 3)
+                {
                     bag3.clear();
                     bag4.clear();
                     bag3.add("A");
@@ -196,6 +254,9 @@ public class BagDriver {
                     bag4.add("B");
                     bag4.add("B");
 
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag3.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag4.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -204,8 +265,10 @@ public class BagDriver {
                     System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
-
-                } else if (choice2 == 4) {
+                }
+                // check to see if the user wishes to perform the operations on two distinct bags.
+                else if (choice2 == 4)
+                {
                     bag3.clear();
                     bag4.clear();
                     bag3.add("A");
@@ -217,18 +280,9 @@ public class BagDriver {
                     bag4.add("G");
                     bag4.add("H");
 
-                    System.out.print("Result of Union: ");
-                    System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
-                    System.out.print("Result of Intersection: ");
-                    System.out.println(Arrays.toString(bag3.intersection(bag4).toArray()));
-                    System.out.print("Result of Difference from the first bag to the second: ");
-                    System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
-                    System.out.print("Result of Difference from the second bag to the first: ");
-                    System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
-                } else if (choice2 == 5) {
-                    bag3.clear();
-                    bag4.clear();
-
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag3.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag4.toArray()));
                     System.out.print("Result of Union: ");
                     System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
                     System.out.print("Result of Intersection: ");
@@ -238,16 +292,33 @@ public class BagDriver {
                     System.out.print("Result of Difference from the second bag to the first: ");
                     System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
                 }
-                else if (choice2 == 6) {
+                // check to see if the user wishes to perform the operations on two empty bags.
+                else if (choice2 == 5)
+                {
                     bag3.clear();
                     bag4.clear();
 
-                    System.out.println("Goodbye");
+                    System.out.println("Displaying the contents...");
+                    System.out.println("Bag #1: " + Arrays.toString(bag3.toArray()));
+                    System.out.println("Bag #2: " + Arrays.toString(bag4.toArray()));
+                    System.out.print("Result of Union: ");
+                    System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
+                    System.out.print("Result of Intersection: ");
+                    System.out.println(Arrays.toString(bag3.intersection(bag4).toArray()));
+                    System.out.print("Result of Difference from the first bag to the second: ");
+                    System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
+                    System.out.print("Result of Difference from the second bag to the first: ");
+                    System.out.println(Arrays.toString(bag4.difference(bag3).toArray()));
+                }
+                // check to see if the user wishes to terminate the program.
+                else if (choice2 == 6)
+                {
+                    bag3.clear();
+                    bag4.clear();
+                    System.out.println("Terminating the program... Goodbye!");
                     break;
                 }
-
-
             }
-        }
-    }
-}
+        } // end of for loop.
+    } // end of main.
+} // end of "BagDriver"
